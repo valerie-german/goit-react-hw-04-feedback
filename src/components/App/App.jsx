58f14a13f425ffa71285182a59966695425fc41d
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-08-28 01:24:55 +0300

    hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,69 +1,96 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  static defaultProps = {
-    initialGood: 0,
-    initialNeutral: 0,
-    initialBad: 0,
+export function App () {
+  // static defaultProps = {
+  //   initialGood: 0,
+  //   initialNeutral: 0,
+  //   initialBad: 0,
+  // };
+
+  // static propTypes = {
+  //   initialGood: PropTypes.number.isRequired,
+  //   initialNeutral: PropTypes.number.isRequired,
+  //   initialBad: PropTypes.number.isRequired,
+  // };
+
+  // state = {
+  //   good: this.props.initialGood,
+  //   neutral: this.props.initialNeutral,
+  //   bad: this.props.initialBad,
+  // };
+
+  const[good, setGood]=useState(0);
+  const[neutral, setNeutral]=useState(0);
+  const[bad, setBad]=useState(0);
+
+
+
+  const handleIncrement = option => {
+    switch (option) {
+      case "good":
+        setGood(good + 1);
+        break;
+    
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+    
+      case "bad":
+        setBad(bad +1);
+        break;
+    
+      default:
+        return;
+    }
+    
   };
 
-  static propTypes = {
-    initialGood: PropTypes.number.isRequired,
-    initialNeutral: PropTypes.number.isRequired,
-    initialBad: PropTypes.number.isRequired,
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  state = {
-    good: this.props.initialGood,
-    neutral: this.props.initialNeutral,
-    bad: this.props.initialBad,
+ const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  handleIncrement = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
+   const namesArray = ["good", "neutral", "bad"];
+   
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
-
-  render() {
-    const namesArray = Object.keys(this.state);
     return (
       <div>
         <Section title="Please leave your feedback">
           <FeedbackOptions
             options={namesArray}
-            onHandleIncrement={this.handleIncrement}
+            onHandleIncrement={handleIncrement}
           />
         </Section>
         <Section title="Statistic">
-          {this.countTotalFeedback() === 0 ? (
+          {countTotalFeedback() === 0 ? (
             <Notification message="There is no feedback" />
           ) : (
             <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              percent={this.countPositiveFeedbackPercentage()}
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              percent={countPositiveFeedbackPercentage()}
             />
           )}
         </Section>
       </div>
     );
   }
-}
+
+
+  App.propTypes = {
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+};
